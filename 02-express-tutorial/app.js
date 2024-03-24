47c19d78cc2638e009d2fea17536eadc93feee65
Doina Fitchevici <domna16@gmail.com>
2024-03-24 07:05:15 -0400

    Implemented GET and POST APIs, and requested body parsing

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { products } = require("./data");
+const { products, people } = require("./data");
 const app = express();
 
 console.log("Express Tutorial");
 
-app.use(express.static("./public"));
+//Middleware to parse request bodies
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static("./methods-public"));
 
 app.get("/api/v1/test", (req, res) => {
   res.json({ message: "It worked!" });
@@ -56,6 +60,26 @@ app.get("/api/v1/query", (req, res) => {
   return res.status(200).json(sortedProducts);
 });
 
+// Implementing GET endpoint for /api/v1/people
+app.get("/api/v1/people", (req, res) => {
+  res.json(people);
+});
+
+// Implementing POST endpoint for /api/v1/people to add an entry to the people array
+app.post("/api/v1/people", (req, res) => {
+  if (!req.body.name) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide a name" });
+  }
+  const newPerson = {
+    id: people.length + 1,
+    name: req.body.name,
+  };
+  people.push(newPerson);
+  res.status(201).json({ success: true, name: req.body.name });
+});
+
 app.all("*", (req, res) => {
   res.status(404).send("Page not found");
 });
@@ -64,10 +88,18 @@ app.all("*", (req, res) => {
 const logger = (req, res, next) => {
   const method = req.method;
   const url = req.url;
-  const date = new Date().getFullYear();
-  console.log(method, url, date);
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
   next();
 };
+
+app.get("/", logger, (req, res) => {
+  res.send("Home");
+});
+
+app.get("/about", logger, (req, res) => {
+  res.send("About");
+});
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
