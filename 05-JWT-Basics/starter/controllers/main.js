253c9d44e13ba5e1826cd5ca3d207b8673e83336
Doina Fitchevici <domna16@gmail.com>
2024-05-11 17:50:47 -0400

    throw error if username and/or password not provided

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,15 +1,25 @@
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  //mongoose validation
+  //Joi
+  //check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
   res.send("Fake login/Register/Signup route");
 };
 
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
-  res
-    .status(200)
-    .json({
-      msg: `Hello. Doina F`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
+  res.status(200).json({
+    msg: `Hello, Doina F`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
